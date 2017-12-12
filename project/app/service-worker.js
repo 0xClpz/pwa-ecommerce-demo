a1728fca53a97df0e4cf354dfd633cb76564c391
Antonio Calapez <antonio@hackages.io>
2017-12-12 15:55:13 +0000

    add sw

--- project/app/service-worker.js
+++ project/app/service-worker.js
@@ -22,3 +22,58 @@ limitations under the License.
 // fetch event listener
 
 // TODO SW-5 - delete outdated caches in the activate event listener
+var filesToCache = [
+    '/',
+    'index.html',
+    'scripts/main.min.js',
+    'styles/main.css',
+    'images/products/BarrelChair.jpg',
+    'images/products/C10.jpg',
+    'images/products/Cl2.jpg',
+    'images/products/CP03_blue.jpg',
+    'images/products/CPC_RECYCLED.jpg',
+    'images/products/CPFS.jpg',
+    'images/products/CPO2_red.jpg',
+    'images/products/CPT.jpg',
+    'images/products/CS1.jpg',
+    'images/touch/apple-touch-icon.png',
+    'images/touch/chrome-touch-icon-192x192.png',
+    'images/touch/icon-128x128.png',
+    'images/touch/ms-touch-icon-144x144-precomposed.png',
+    'images/about-hero-image.jpg',
+    'images/delete.svg',
+    'images/footer-background.png',
+    'images/hamburger.svg',
+    'images/header-bg.jpg',
+    'images/logo.png'];
+  
+  var staticCacheName = 'e-commerce-v1';
+  
+  self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+
+  self.addEventListener('activate', function(event) {
+    console.log('Activating new service worker...');
+  
+    var cacheWhitelist = [staticCacheName];
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
